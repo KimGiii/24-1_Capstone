038d3e377bf1f17f1f682d157e9c4bd89df029bc
Gibo Kim <97424608+KimGiii@users.noreply.github.com>
2024-10-10 23:14:33 +0900

    Add files via upload

---
+++ mediapipe_unity.py
@@ -0,0 +1,132 @@
+import math
+import cv2
+import numpy as np
+from time import time
+import mediapipe as mp
+import matplotlib.pyplot as plt
+import socket
+def detectPose(image, pose, display=True):
+    '''
+    This function performs pose detection on an image.
+    Args:
+        image: The input image with a prominent person whose pose landmarks needs to be detected.
+        pose: The pose setup function required to perform the pose detection.
+        display: A boolean value that is if set to true the function displays the original input image, the resultant image,
+                 and the pose landmarks in 3D plot and returns nothing.
+    Returns:
+        output_image: The input image with the detected pose landmarks drawn.
+        landmarks: A list of detected landmarks converted into their original scale.
+    '''
+    # 예시이미지 copy하기
+    output_image = image.copy()
+
+    # 컬러 이미지 BGR TO RGB 변환
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # pose detection 수행
+    results = pose.process(imageRGB)
+
+    # input image의 너비&높이 탐색
+    height, width, _ = image.shape
+
+    # detection landmarks를 저장할 빈 list 초기화
+    landmarks = []
+
+    # landmark가 감지 되었는지 확인
+    if results.pose_landmarks:
+
+        # landmark 그리기
+        mp_drawing.draw_landmarks(image=output_image, landmark_list=results.pose_landmarks,
+                                  connections=mp_pose.POSE_CONNECTIONS)
+
+        # 감지된 landmark 반복
+        for landmark in results.pose_landmarks.landmark:
+            # landmark를 list에 추가하기
+            landmarks.append((int(landmark.x * width), int(landmark.y * height), (landmark.z * width)))
+
+    # 오리지널 image와 pose detect된 image 비교
+    if display:
+
+      # 오리지널 & 아웃풋 이미지 그리기
+      plt.figure(figsize=[22,22])
+      plt.subplot(121);plt.imshow(image[:,:,::-1]);plt.title("Original Image");plt.axis('off');
+      plt.subplot(122);plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
+
+      # 3D 랜드마크 나타내기ㅂ
+      mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+
+    # 그렇지 않다면, output_image 와 landmark return한다
+    else:
+
+      return output_image, landmarks
+
+
+# Initializing mediapipe pose class.
+# mediapipe pose class를 초기화 한다.
+mp_pose = mp.solutions.pose
+
+# Setting up the Pose function.
+# pose detect function에 image detect=True, 최소감지신뢰도 = 0.3, 모델 복잡도 =2를 준다.
+pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.3, model_complexity=2)
+
+# Initializing mediapipe drawing class, useful for annotation.
+# mediapipe의 drawing class를 초기화한다.
+mp_drawing = mp.solutions.drawing_utils
+
+# Setup Pose function for video.
+pose_video = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, model_complexity=1)
+
+# Initialize the VideoCapture object to read from the webcam.
+camera_video = cv2.VideoCapture(0)
+
+# Initialize a resizable window.
+cv2.namedWindow('Pose Classification', cv2.WINDOW_NORMAL)
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1", 5052)
+
+# Iterate until the webcam is accessed successfully.
+while camera_video.isOpened():
+
+    # Read a frame.
+    ok, frame = camera_video.read()
+
+    # Check if frame is not read properly.
+    if not ok:
+        # Continue to the next iteration to read the next frame and ignore the empty camera frame.
+        continue
+
+    # Flip the frame horizontally for natural (selfie-view) visualization.
+    frame = cv2.flip(frame, 1)
+
+    # Get the width and height of the frame
+    frame_height, frame_width, _ = frame.shape
+
+    # Resize the frame while keeping the aspect ratio.
+    frame = cv2.resize(frame, (int(frame_width * (640 / frame_height)), 640))
+
+    # Perform Pose landmark detection.
+    frame, landmarks = detectPose(frame, pose_video, display=False)
+    data = []
+
+    if landmarks:
+        for lm in landmarks:
+            data.extend([lm[0], frame_height - lm[1], lm[2]])
+        sock.sendto(str.encode(str(data)), serverAddressPort)
+
+    # Display the frame.
+    frame = cv2.resize(frame,(0,0),None,0.5,0.5)
+    cv2.imshow('Pose Classification', frame)
+
+    # Wait until a key is pressed.
+    # Retreive the ASCII code of the key pressed
+    k = cv2.waitKey(1) & 0xFF
+
+    # Check if 'ESC' is pressed.
+    if (k == 27):
+        # Break the loop.
+        break
+
+# Release the VideoCapture object and close the windows.
+camera_video.release()
+cv2.destroyAllWindows()
